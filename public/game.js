2ef1f540c222716fa7f8f6870b7b6171f756b87f
Adrian-Avalos <adrianpiti28@gmail.com>
2025-04-10 10:32:12 -0400

    Update game.js

--- public/game.js
+++ public/game.js
@@ -4,10 +4,16 @@ const ctx = canvas.getContext('2d');
 const tileSize = 20;
 let playerId;
 let players = {};
+let fruit = {};
+let bomb = {};
+let speedBoost = {};
 
 socket.on('init', (data) => {
   playerId = data.id;
   players = data.players;
+  fruit = data.fruit;
+  bomb = data.bomb;
+  speedBoost = data.speedBoost;
 });
 
 socket.on('new-player', ({ id, data }) => {
@@ -18,13 +24,21 @@ socket.on('remove-player', (id) => {
   delete players[id];
 });
 
-socket.on('state', (serverPlayers) => {
-  players = serverPlayers;
+socket.on('state', (data) => {
+  players = data.players;
+  fruit = data.fruit;
+  bomb = data.bomb;
+  speedBoost = data.speedBoost;
   draw();
 });
 
 document.addEventListener('keydown', (e) => {
-  const dir = { ArrowUp: [0, -1], ArrowDown: [0, 1], ArrowLeft: [-1, 0], ArrowRight: [1, 0] };
+  const dir = {
+    ArrowUp: [0, -1],
+    ArrowDown: [0, 1],
+    ArrowLeft: [-1, 0],
+    ArrowRight: [1, 0]
+  };
   if (dir[e.key]) {
     socket.emit('move', { dx: dir[e.key][0], dy: dir[e.key][1] });
   }
@@ -33,10 +47,22 @@ document.addEventListener('keydown', (e) => {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  // Power-ups
+  drawTile(fruit, 'green');
+  drawTile(bomb, 'red');
+  drawTile(speedBoost, 'cyan');
+
+  // Jugadores
   Object.values(players).forEach(p => {
+    if (!p.alive) return;
     ctx.fillStyle = p.color;
     p.tail.forEach(part => {
-      ctx.fillRect(part.x * tileSize, part.y * tileSize, tileSize, tileSize);
+      drawTile(part, p.color);
     });
   });
+}
+
+function drawTile(pos, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(pos.x * tileSize, pos.y * tileSize, tileSize, tileSize);
 }

79adfba70c1f717c846fbaccfcf2a633de439e7b
Adrian-Avalos <adrianpiti28@gmail.com>
2025-04-10 10:50:17 -0400

    Update game.js

--- public/game.js
+++ public/game.js
@@ -4,9 +4,7 @@ const ctx = canvas.getContext('2d');
 const tileSize = 20;
 let playerId;
 let players = {};
-let fruit = {};
-let bomb = {};
-let speedBoost = {};
+let fruit = {}, bomb = {}, speedBoost = {};
 
 socket.on('init', (data) => {
   playerId = data.id;
@@ -33,36 +31,30 @@ socket.on('state', (data) => {
 });
 
 document.addEventListener('keydown', (e) => {
-  const dir = {
-    ArrowUp: [0, -1],
-    ArrowDown: [0, 1],
-    ArrowLeft: [-1, 0],
-    ArrowRight: [1, 0]
+  const dirs = {
+    ArrowUp: [0, -1], ArrowDown: [0, 1],
+    ArrowLeft: [-1, 0], ArrowRight: [1, 0]
   };
-  if (dir[e.key]) {
-    socket.emit('move', { dx: dir[e.key][0], dy: dir[e.key][1] });
+  if (dirs[e.key]) {
+    socket.emit('move', { dx: dirs[e.key][0], dy: dirs[e.key][1] });
   }
 });
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawItem(fruit, 'lime');
+  drawItem(bomb, 'red');
+  drawItem(speedBoost, 'cyan');
 
-  // Power-ups
-  drawTile(fruit, 'green');
-  drawTile(bomb, 'red');
-  drawTile(speedBoost, 'cyan');
-
-  // Jugadores
-  Object.values(players).forEach(p => {
-    if (!p.alive) return;
+  for (const id in players) {
+    const p = players[id];
+    if (!p.alive) continue;
     ctx.fillStyle = p.color;
-    p.tail.forEach(part => {
-      drawTile(part, p.color);
-    });
-  });
+    p.tail.forEach(part => drawItem(part, p.color));
+  }
 }
 
-function drawTile(pos, color) {
+function drawItem(pos, color) {
   ctx.fillStyle = color;
   ctx.fillRect(pos.x * tileSize, pos.y * tileSize, tileSize, tileSize);
 }
